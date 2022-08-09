5a4976c93e5aaa297a153dde3de14e6024dcaaa0
Jafrin Akter Alo <81787520+JafrinAlo@users.noreply.github.com>
2022-08-09 19:57:30 +0600

    Add files via upload

---
+++ 96A_Football.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin>>str;
+
+    int ans=0;
+    for(int i=0;i<str.length()-1;i++)
+    {
+         int count=-1;
+        //cout<<i<<" "<<str[i]<<endl;
+        for(int j=i;j<str.length();j++){
+            if(str[i]==str[j])
+            {
+               // cout<<"i: "<<i<<" "<<str[i]<<" j: "<<j<<" "<<str[j]<<endl;
+                count++;
+                //cout<<"count: "<<count<<endl;
+                //cout<<endl;
+                //cout<<endl;
+                if(count==6){
+                        ans++;
+                break;
+                }
+            }
+           else if(str[i]!=str[j])
+             {
+                 break;
+             }
+        }
+    }
+   // if(ans>0)cout<<"YES ans: "<<ans<<endl;
+   if(ans>0)cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+}
