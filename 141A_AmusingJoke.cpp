0f8232a3f2ec5471e36b5efc2192564751001b6d
Jafrin Akter Alo <81787520+JafrinAlo@users.noreply.github.com>
2022-12-08 13:02:18 +0600

    Add files via upload

--- 141A_AmusingJoke.cpp
+++ 141A_AmusingJoke.cpp
@@ -1,36 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+string sortString(string &str)
+{
+    sort(str.begin(), str.end());
+    return str;
+}
+
 int main()
 {
-    string s1;
-    string s2;
-    string s3;
-    int countAdd12,countAdd3=0;
-    int n12,n3=0;
-
+    string s1, s2,s3;
     cin>>s1>>s2>>s3;
-    string s12=s1.append(s2);
 
-    set<char> str12;
-    set<char> str3;
-    set<char>::iterator itr;
-    for(itr=str12.begin();itr<str12.end();itr++)
-    {
-        str12.insert(s12[i]);
-        n12=n12+*itr;
-    }
+    string s12=s1+s2;
 
-     for(itr=str3.begin();itr<str3.end();itr++)
-    {
-        str3.insert(s3[i]);
-        n3=n3+*itr;
-    }
+    string str12=sortString(s12);
+    string str3=sortString(s3);
 
-    if((n12==n3)&& (s12-str12.size())==(s3-str3.size())){
-            cout<<n1<<" "<<n2<<" "<<n3<<endl;
+    //cout<<str12<<endl;
+    //cout<<str3<<endl;
+
+    if(str12==str3){
         cout<<"YES";
     }
     else
         cout<<"NO";
+
+        return 0;
 }
