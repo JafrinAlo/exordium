90f816fc8b9936a535e0ae4486462a16ffc07e06
Jafrin Akter Alo <81787520+JafrinAlo@users.noreply.github.com>
2022-12-16 19:21:14 +0600

    Add files via upload

---
+++ 1475B_NewYearNumber.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+int main()
+{
+    fastread();
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        int mod=n%2020;
+        int div=n/2020;
+
+        if( mod <= div){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
